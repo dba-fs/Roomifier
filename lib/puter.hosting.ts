ba9b2ab289e723e9094048da59540d84da00204b
Dani <danielbarberoarroyo@gmail.com>
2026-07-27 23:00:10 +0200

    fix(coderabbit): implementing suggested fixes

--- lib/puter.hosting.ts
+++ lib/puter.hosting.ts
@@ -11,7 +11,10 @@ export const getOrCreateHostingConfig= async (): Promise<HostingConfig | null> =
     try {
         const created = await puter.hosting.create(subdomain, '.');
 
-        return {subdomain: created.subdomain};
+        const record = { subdomain: created.subdomain};
+
+        await puter.kv.set(HOSTING_CONFIG_KEY, { subdomain: created.subdomain });
+        return record;
     } catch (e) { 
         console.warn(`Could not find subdomainm: ${e}`);
         return null;
